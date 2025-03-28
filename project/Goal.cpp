24b78af3b72523ac6b3892ef56d39d98a3997ab8
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-03-28 11:09:59 +0900

    コメントアウトを追加

--- project/Goal.cpp
+++ project/Goal.cpp
@@ -1,86 +1,74 @@
 ﻿#include "Goal.h"
-#include<iostream>
-
+#include <iostream>
 #include "externels/imgui/imgui.h"
 #include "externels/imgui/imgui_impl_dx12.h"
 #include "externels/imgui/imgui_impl_win32.h"
 
-
-//デストラクタ
+// デストラクタ
 Goal::~Goal() {
-	delete goalModel_;
-	delete clearSprite_;
-
+	delete goalModel_;     // ゴールオブジェクトのメモリ解放
+	delete clearSprite_;   // クリアスプライトのメモリ解放
 }
 
-void Goal::Initialize(Vector3 position, DirectXBase* dxc)
-{
+// 初期化処理
+// @param position: ゴールの位置
+// @param dxc: DirectXの基盤クラス
+void Goal::Initialize(Vector3 position, DirectXBase* dxc) {
 	directX = dxc;
 	goalPos_ = position;
 
+	// 各マネージャーの初期化
 	ModelManager::GetInstance()->Initialize(directX);
 	Object3dBase::GetInstance()->Initialize(directX);
 	ModelBase::GetInstance()->Initialize(directX);
 
-	//モデル読み込み
+	// モデルの読み込み
 	ModelManager::GetInstance()->LoadModel("Resources/Model", "axis.obj");
 	ModelManager::GetInstance()->LoadModel("Resources/Model", "goal.obj");
 	ModelManager::GetInstance()->LoadModel("Resources/Model", "box.obj", true);
 
-
-	// object3dの初期化
+	// Object3dの初期化
 	goalModel_ = new Object3d();
 	goalModel_->Initialize();
 	goalModel_->SetModel("goal.obj");
-	goalModel_->SetTranslate(goalPos_);//位置を指定する
+	goalModel_->SetTranslate(goalPos_); // ゴール位置を設定
 
-	//// クリアスプライトの初期化
-	//clearSprite_ = new Sprite();
-	//clearSprite_->Initialize("Resources/Texture/clear.png");
-	//clearSprite_->SetPosition({ 320, 180 });  // 画面中央
-
+	// クリアスプライトの初期化（コメントアウトされているが後で使用する可能性あり）
+	// clearSprite_ = new Sprite();
+	// clearSprite_->Initialize("Resources/Texture/clear.png");
+	// clearSprite_->SetPosition({ 320, 180 });  // 画面中央に配置
 }
 
-void Goal::Update()
+// 更新処理
+void Goal::Update() {
+	goalModel_->Update(); // ゴールオブジェクトの更新
 
-{
-	goalModel_->Update();
-
-	////ゴールすると trueになる => アニメーションの処理追加
-	//if (!goalFlag_) {
-	//	if () {			//playerと当たったら
-	//		goalFlag_ = true;
-	//	}			// ゴールフラグが trueならクリアスプライトを描画
-	//	if (goalFlag_) {
-	//		clearSprite_->Draw();
-	//	}
-	//}
-
-	// ImGuiウィンドウの中にチェックボックスを追加
+	// ImGuiを用いたデバッグ用UI
 	ImGui::Begin("Goal Window");
-	ImGui::DragFloat3("Goal Position", &goalPos_.x, 0.01f); //ゴールの位置の確認
-	ImGui::Checkbox("Goal Flag", &goalFlag_);
+	ImGui::DragFloat3("Goal Position", &goalPos_.x, 0.01f); // ゴールの位置確認
+	ImGui::Checkbox("Goal Flag", &goalFlag_); // ゴールフラグの確認
 	ImGui::End();
-
-
 }
 
+// 描画処理
+// @param directionalLightResource: 平行光源のリソース
+// @param pointLightResource: 点光源のリソース
+// @param spotLightResource: スポットライトのリソース
+void Goal::Draw(Microsoft::WRL::ComPtr<ID3D12Resource> directionalLightResource,
+	Microsoft::WRL::ComPtr<ID3D12Resource> pointLightResource,
+	Microsoft::WRL::ComPtr<ID3D12Resource> spotLightResource) {
 
+	goalModel_->Draw(directionalLightResource, pointLightResource, spotLightResource); // ゴールオブジェクトの描画
 
-void Goal::Draw(Microsoft::WRL::ComPtr<ID3D12Resource>directionalLightResource, Microsoft::WRL::ComPtr<ID3D12Resource>pointLightResource, Microsoft::WRL::ComPtr<ID3D12Resource>spotLightResource) {
-
-	goalModel_->Draw(directionalLightResource, pointLightResource, spotLightResource);
-
-	//// ゴールフラグがtrueならクリアスプライトを描画
-	//if (goalFlag_) {
+	// ゴールフラグがtrueならクリアスプライトを描画（現在はコメントアウト）
+	// if (goalFlag_) {
 	//	clearSprite_->Draw();
-	//}
-
+	// }
 }
 
-
+// プレイヤーとの当たり判定
+// @param player: 衝突を確認するプレイヤーオブジェクト
 void Goal::OnCollision(const Player* player) {
-	(void)player;
-	//goalFlag_ = true;  // プレイヤーと当たったらゴールフラグをtrueにする
-
+	(void)player; // 未使用警告を回避するためのキャスト
+	// goalFlag_ = true;  // プレイヤーと当たったらゴールフラグをtrueにする（現在コメントアウト）
 }
--- project/Goal.h
+++ project/Goal.h
@@ -1,51 +1,55 @@
 ﻿#pragma once
-#include"kMath.h"
+#include "kMath.h"
 #include "Vector3.h" 
-
 #include "Model.h"
-#include"Camera.h"
+#include "Camera.h"
 #include "Sprite.h"
+#include "Object3d.h"
+#include "Object3dBase.h"
+#include "ModelBase.h"
+#include "ModelManager.h"
+#include "DirectXBase.h"
 
-#include"Object3d.h"
-#include"Object3dBase.h"
-#include"ModelBase.h"
-#include"ModelManager.h"
+class Player; // プレイヤークラスの前方宣言（後で追加）
 
-#include"DirectXBase.h"
-
-
-class Player;//後で追加
-
-class Goal
-{
+// ゴールクラス
+class Goal {
 public:
-
+	// デストラクタ
 	~Goal();
-	// 初期化
-	void Initialize(Vector3, DirectXBase*); //Object3d*
-	void Update();
-	//描画
-	void Draw(Microsoft::WRL::ComPtr<ID3D12Resource>, Microsoft::WRL::ComPtr<ID3D12Resource>, Microsoft::WRL::ComPtr<ID3D12Resource>);
 
-	//当たり判定
+	// 初期化処理
+	// @param position: ゴールの位置
+	// @param dxc: DirectXの基盤クラス
+	void Initialize(Vector3 position, DirectXBase* dxc);
+
+	// 更新処理
+	void Update();
+
+	// 描画処理
+	// @param 第1引数: 平行光源のリソース
+	// @param 第2引数: 点光源のリソース
+	// @param 第3引数: スポットライトのリソース
+	void Draw(Microsoft::WRL::ComPtr<ID3D12Resource> directionalLightResource,
+		Microsoft::WRL::ComPtr<ID3D12Resource> pointLightResource,
+		Microsoft::WRL::ComPtr<ID3D12Resource> spotLightResource);
+
+	// 当たり判定
+	// @param player: 衝突を確認するプレイヤーオブジェクト
 	void OnCollision(const Player* player);
 
+	// ゴール到達フラグを取得
+	// @return ゴール到達状態（true: ゴール済み, false: 未到達）
 	bool GetFlag() { return goalFlag_; }
 
 private:
+	DirectXBase* directX;   // DirectXの基盤クラス
+	Object3d* goalModel_;   // ゴールの3Dオブジェクトモデル
+	Vector3 goalPos_;       // ゴールの位置
+	Camera* goalCamera_;    // ゴール用のカメラ
 
-	//DirectX
-	DirectXBase* directX;
-	Object3d* goalModel_;	// objモデルデータ
-	Vector3 goalPos_;		// ゴールの位置
-	Camera* goalCamera_;	//カメラ
+	bool goalFlag_ = false; // ゴール到達フラグ
 
-	//終了フラグ
-	bool goalFlag_ = false;
-
-	//ゴール
-	Goal* goal_ = nullptr;
-
-	Sprite* clearSprite_ = nullptr; // クリアスプライト
+	Goal* goal_ = nullptr;  // ゴールオブジェクト（※不要な可能性あり）
+	Sprite* clearSprite_ = nullptr; // クリア画面用のスプライト
 };
-
