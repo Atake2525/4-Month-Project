46faa80ea4ec61b580f09cc1b9f96a1a537ea46e
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-05-29 10:45:34 +0900

    effect-ongroudのみ

--- project/Application/Player.cpp
+++ project/Application/Player.cpp
@@ -144,12 +144,13 @@ void Player::Update()
 
 	//camera_->SetTranslate(cameraTransform_.translate);
 	
-	
+	if (onGround_) {
 		if (input_->TriggerKey(DIK_SPACE)) {
 			//effect
 			effectFlag = true;
 			effectTimer = 5;
 		}
+	}
 	
 	if (effectFlag) {
 		/*位置*/
